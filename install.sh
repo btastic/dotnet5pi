5350c84e8e97e9731f708be4ccad306af684a017
Ben Konsemüller <bkonsem@gmail.com>
2021-07-10 13:00:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -145,11 +145,11 @@ echo -e "\e[1m    Make Link Permanent"
 echo -e "\e[1m----------------------------------------"
 echo -e "\e[0m"
 
-if grep -q 'export DOTNET_ROOT=' /home/pi/.bashrc;  then
+if grep -q 'export DOTNET_ROOT=' /root/.bashrc;  then
   echo 'Already added link to .bashrc'
 else
   echo 'Adding Link to .bashrc'
-  echo 'export DOTNET_ROOT=/opt/dotnet' >> /home/pi/.bashrc
+  echo 'export DOTNET_ROOT=/opt/dotnet' >> /root/.bashrc
 fi
 
 echo -e "\e[0m"
@@ -160,8 +160,8 @@ echo -e "\e[0m"
 
 cd ~
 
-wget -O /home/pi/dotnetdebug.sh https://raw.githubusercontent.com/pjgpetecodes/dotnet5pi/master/dotnetdebug.sh
-chmod +x /home/pi/dotnetdebug.sh 
+wget -O /root/dotnetdebug.sh https://raw.githubusercontent.com/pjgpetecodes/dotnet5pi/master/dotnetdebug.sh
+chmod +x /root/dotnetdebug.sh 
 
 echo -e "\e[0m"
 echo -e "\e[1m----------------------------------------"
